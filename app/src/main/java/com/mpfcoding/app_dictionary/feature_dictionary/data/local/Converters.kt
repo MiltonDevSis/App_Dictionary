c29d3db1f299372d453f31544f39d358f4c15a90
MiltonDevSis <milton.dev.sis@gmail.com>
2021-11-21 11:08:19 -0300

    Add converter json for the database

---
+++ app/src/main/java/com/mpfcoding/app_dictionary/feature_dictionary/data/local/Converters.kt
@@ -0,0 +1,29 @@
+package com.mpfcoding.app_dictionary.feature_dictionary.data.local
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.google.gson.reflect.TypeToken
+import com.mpfcoding.app_dictionary.feature_dictionary.data.remote.util.JsonParser
+import com.mpfcoding.app_dictionary.feature_dictionary.domain.model.Meaning
+
+@ProvidedTypeConverter
+class Converters(
+    private val jsonParser: JsonParser
+) {
+
+    @TypeConverter
+    fun fromMeaningsJson(json: String): List<Meaning>{
+        return jsonParser.fromJson<ArrayList<Meaning>>(
+            json,
+            object : TypeToken<ArrayList<Meaning>>(){}.type
+        ) ?: emptyList()
+    }
+
+    @TypeConverter
+    fun toMeaningJson(meaning: List<Meaning>): String{
+        return jsonParser.toJson(
+            meaning,
+            object : TypeToken<ArrayList<Meaning>>(){}.type
+        ) ?: "[]"
+    }
+}
